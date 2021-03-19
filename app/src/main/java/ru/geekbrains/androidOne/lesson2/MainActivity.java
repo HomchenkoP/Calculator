81e3be5285496a8defd57a66a7459f65cfe6e0eb
HomchenkoP <homchenkop@yandex.ru>
2021-03-19 15:30:43 +0300

    Добавлено главное меню.

--- app/src/main/java/ru/geekbrains/androidOne/lesson2/MainActivity.java
+++ app/src/main/java/ru/geekbrains/androidOne/lesson2/MainActivity.java
@@ -1,12 +1,14 @@
 package ru.geekbrains.androidOne.lesson2;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import ru.geekbrains.androidOne.lesson3.CalculatorModel;
 import ru.geekbrains.androidOne.lesson3.Operator;
@@ -144,5 +146,26 @@ public class MainActivity extends BaseActivity {
         super.onRestoreInstanceState(instanceState);
         engine = instanceState.getParcelable(PARCELABLE_KEY);
         display.setText(engine.getEntry());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // получим идентификатор выбранного пункта меню
+        int id = item.getItemId();
+
+        // Операции для выбранного пункта меню
+        switch (id) {
+            case R.id.action_settings:
+                Toast.makeText(getApplicationContext(), "Выбрано меню настроек", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
